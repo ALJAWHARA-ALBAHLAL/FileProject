9e37ca55c3c39ecde18fa7c01b57639ba4887687
907460 <907460@LPTEMP907460>
2023-01-04 02:41:18 +0300

    Add comments

--- FileProject/Program.cs
+++ FileProject/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.IO;
+using System.IO; //files library  
 
 namespace FileProject
 {
@@ -8,9 +8,16 @@ namespace FileProject
 
         // ----------- In this program I read and write to the file ----------- 
 
+        // I will add methods, one for read , another for write. (SOLID :) )
+        // Pass the path and data to write in a file  by the user to input (ADD) 
+        // Overwrite to the file by the user input (Delete I think)(Not Today) append or overwrite 
+        // View the file, as i switch, while loop to inter input as a choices ADD - Delete - Display
+        // Each method will have try-catch-finally
+        // await we cant read and write in the same time - async, in the same time we can read and write
+
         static void Main(String[] args)
         {
-            String filePath = @"C:\Test\File.txt";
+            String filePath = @"C:\Test\File.txt"; 
 
             FileInfo fileInfo = new FileInfo(filePath);
 
@@ -26,35 +33,41 @@ namespace FileProject
             try
             {
 
-                fileStreamRead = fileInfo.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+                fileStreamRead = fileInfo.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
 
-                fileStreamWrite = fileInfo.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+                fileStreamWrite = fileInfo.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
 
                 streamWriter = new StreamWriter(fileStreamWrite);
 
-                streamReader = new StreamReader(fileStreamWrite);
+                streamReader = new StreamReader(fileStreamRead);
 
                 streamWriter.WriteLine("   New Line");
 
 
-                String fileContent = streamReader.ReadLine();
+                String fileContent = streamReader.ReadLine(); //Read only line 
 
 
-                while (fileContent != null)
+                while (fileContent != null) //When there is a line to read
                 {
                     Console.WriteLine(fileContent);
-                    fileContent = streamReader.ReadLine();
+                    fileContent = streamReader.ReadLine(); 
                     Console.ReadLine();
 
                 }
+
+               /* if(File.Exists(filePath)) { 
+                    Console.WriteLine("File Found"+ filePath);
+                    File.Delete(filePath);
+
+                }  */
             }
-            catch (FileNotFoundException ex)
+            catch (FileNotFoundException ex) //If file not found
             {
                 Console.WriteLine("The file {0} is not found", ex.Message);
                 Console.ReadLine();
 
             }
-            catch (Exception e)
+            catch (Exception e)//Any other Error
             {
                 Console.WriteLine("Ther was a problem" + "\n" + e.Message);
                 Console.ReadLine();
